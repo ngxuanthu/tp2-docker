51b7e1adfe10d7d290a775a7e3c96f97224d2c06
ngxuanthu <ngtr.xuanthu@gmail.com>
2024-10-18 08:17:02 +0200

    Update

--- etape2/app/test_bdd.php
+++ etape2/app/test_bdd.php
@@ -4,15 +4,12 @@ $username = "example_user";
 $password = "example_password";
 $dbname = "example_db";
 
-// Créer une connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérifier la connexion
 if ($conn->connect_error) {
     die("La connexion a échoué: " . $conn->connect_error);
 }
 
-// Insérer une nouvelle ligne dans la table "test"
 $sql = "INSERT INTO test (content) VALUES ('Données aléatoires')";
 if ($conn->query($sql) === TRUE) {
     echo "Nouvelle ligne insérée avec succès<br>";
@@ -20,7 +17,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Erreur : " . $sql . "<br>" . $conn->error;
 }
 
-// Lire les données de la table "test"
 $sql = "SELECT id, content FROM test ORDER BY id DESC LIMIT 1";
 $result = $conn->query($sql);
 
